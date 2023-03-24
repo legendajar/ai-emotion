97432fd6c728abf2ffcc3b6264099b996c7cbebf
Abhishek Kumar <abhishek.kumar05@nagarro.com>
2023-03-24 23:09:43 +0530

    code clean up

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 from flask import Flask, render_template, Response
 from util import *
-import csv
-import random
-import os
-import re
-import spotipy
-from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
  
@@ -29,60 +22,7 @@ def video():
 
 @app.route('/get-songs')
 def getSongs():
-    CLIENT_ID = os.getenv("CLIENT_ID", "011e37396bd5450ab6273510416b5e1f")
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET", "62aafcf856bb4914907d3d6f1db46ae9")
-    PLAYLIST_LINK = "https://open.spotify.com/playlist/0z5GPu1ZL2ryEmPbTyH0tB?si=d42be5c6ec194bb9&nd=1"
-
-    emotions = {
-        'sad': '0z5GPu1ZL2ryEmPbTyH0tB',
-        'angry': '0a4Hr64HWlxekayZ8wnWqx',
-        'happy': '1ZKdvf5yXvwVyzgznVz2Nl',
-        'surprise': '3FDsPHUToNnMClpbcQ1fyj'
-    }
-
-    f_emotion = emotion
-
-    # map facial expression emotion with song genre
-    if f_emotion in emotions:
-        code = emotions[f_emotion]
-
-        PLAYLIST_LINK = f"https://open.spotify.com/playlist/{code}?si=d42be5c6ec194bb9&nd=1"
-    else:
-        print(f"Error: '{f_emotion}' is not a valid emotion.")
-
-    client_credentials_manager = SpotifyClientCredentials(
-        client_id=CLIENT_ID, client_secret=CLIENT_SECRET
-    )
-
-    # create spotify session object
-    session = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-    # get uri from https link
-    if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", PLAYLIST_LINK):   
-        playlist_uri = match.groups()[0]
-
-    else:
-        raise ValueError("Expected format: https://open.spotify.com/playlist/...")
-
-    # get list of tracks in a given playlist (note: max playlist length 100)
-    tracks = session.playlist_tracks(playlist_uri)["items"]
-
-    # extract name and artist
-    playlist = []
-    for track in tracks:
-        name = track["track"]["name"]
-        uri = track["track"]["preview_url"]
-
-        artists = ", ".join([artist["name"] for artist in track["track"]["artists"]])
-        if uri and artists:
-            playlist.append((uri, name, artists))
-
-    random_playlists = random.sample(playlist, 8)
-
-    recommendedPlaylist = {
-        'currentMood': emotion,
-        'playlists': random_playlists
-    }
+    recommendedPlaylist = SpotipyAPI().getSongOnMood(emotion)
 
     return render_template('index.html', recommendedPlaylist=recommendedPlaylist)
 
--- util.py
+++ util.py
@@ -3,11 +3,20 @@ from threading import Thread
 from deepface import DeepFace
 import numpy as np
 from PIL import Image
+import csv
+import random
+import os
+import re
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
 
-global cam 
-facecascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-global last_frame                                  
+global cam
+facecascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+global last_frame
 last_frame = np.zeros((480, 640, 3), dtype=np.uint8)
+
 
 class LiveVedioFeed:
 
@@ -47,7 +56,8 @@ class VideoCamera(object):
         cam = LiveVedioFeed(src=0).start()
         frame = cam.read()
 
-        result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
+        result = DeepFace.analyze(
+            frame, actions=['emotion'], enforce_detection=False)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = facecascade.detectMultiScale(gray, 1.1, 4)
@@ -55,7 +65,8 @@ class VideoCamera(object):
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(frame, result[0]['dominant_emotion'], (x+int(w/3), y), cv2.FONT_ITALIC, 1, (246, 6, 13), 2)
+            cv2.putText(frame, result[0]['dominant_emotion'],
+                        (x+int(w/3), y), cv2.FONT_ITALIC, 1, (246, 6, 13), 2)
 
         global last_frame
         last_frame = frame.copy()
@@ -64,3 +75,69 @@ class VideoCamera(object):
         ret, buffer = cv2.imencode('.jpg', img)
         emotion = result[0]['dominant_emotion']
         return buffer.tobytes(), emotion
+
+
+class SpotipyAPI(object):
+
+    def getSongOnMood(self, mood):
+
+        CLIENT_ID = os.getenv("CLIENT_ID", "011e37396bd5450ab6273510416b5e1f")
+        CLIENT_SECRET = os.getenv(
+            "CLIENT_SECRET", "62aafcf856bb4914907d3d6f1db46ae9")
+        PLAYLIST_LINK = "https://open.spotify.com/playlist/0z5GPu1ZL2ryEmPbTyH0tB?si=d42be5c6ec194bb9&nd=1"
+
+        emotions = {
+            'sad': '0z5GPu1ZL2ryEmPbTyH0tB',
+            'angry': '0a4Hr64HWlxekayZ8wnWqx',
+            'happy': '1ZKdvf5yXvwVyzgznVz2Nl',
+            'surprise': '3FDsPHUToNnMClpbcQ1fyj'
+        }
+
+        f_emotion = mood
+
+        # map facial expression emotion with song genre
+        if f_emotion in emotions:
+            code = emotions[f_emotion]
+
+            PLAYLIST_LINK = f"https://open.spotify.com/playlist/{code}?si=d42be5c6ec194bb9&nd=1"
+        else:
+            print(f"Error: '{f_emotion}' is not a valid emotion.")
+
+        client_credentials_manager = SpotifyClientCredentials(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET
+        )
+
+        # create spotify session object
+        session = spotipy.Spotify(
+            client_credentials_manager=client_credentials_manager)
+
+        # get uri from https link
+        if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", PLAYLIST_LINK):
+            playlist_uri = match.groups()[0]
+
+        else:
+            raise ValueError(
+                "Expected format: https://open.spotify.com/playlist/...")
+
+        # get list of tracks in a given playlist (note: max playlist length 100)
+        tracks = session.playlist_tracks(playlist_uri)["items"]
+
+        # extract name and artist
+        playlist = []
+        for track in tracks:
+            name = track["track"]["name"]
+            uri = track["track"]["preview_url"]
+
+            artists = ", ".join([artist["name"]
+                                for artist in track["track"]["artists"]])
+            if uri and artists:
+                playlist.append((uri, name, artists))
+
+        random_playlists = random.sample(playlist, 8)
+
+        recommendedPlaylist = {
+            'currentMood': mood,
+            'playlists': random_playlists
+        }
+
+        return recommendedPlaylist
